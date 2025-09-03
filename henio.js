d20c139b85a22fb8930ee9f5b52c8486eed02230
traaays <101186150+traaays@users.noreply.github.com>
2025-09-03 12:37:16 +0200

    Babel

--- henio.js
+++ henio.js
@@ -1,21 +1,23 @@
-// 0 = idle, 1 = smack, 2 = babel
+// 0 = idle, 1 = smack
 var henioState = 0
 
 const imgHenio = document.getElementById("henioImg");
 const aHenio = document.getElementById("henio")
+
 const smack = document.getElementById('smack')
 const babel = document.getElementById('babel')
+const splat = document.getElementById('splat')
 
 imgHenio.addEventListener("click", () => {
     if(henioState == 0){
-        imgHenio.src = "assets/babel/babelpunch.png"
+        imgHenio.src = "assets/babel/withTP/babelpunch.png"
         smack.play()
         henioState = henioState + 1
         setTimeout(() => {
-            imgHenio.src = "assets/babel/babeltalk.png";
+            imgHenio.src = "assets/babel/withTP/babeltalk.png";
             babel.play()
             setTimeout(() => {
-                imgHenio.src = "assets/babel/babelstand.png";
+                imgHenio.src = "assets/babel/withTP/babelstand.png";
                 henioState = 0
             }, 800);
         }, 800);        
