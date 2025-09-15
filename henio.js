89a3593e885e2f29e9d3213b7f8f9692d28df48a
traaays <101186150+traaays@users.noreply.github.com>
2025-09-16 01:43:47 +0200

    nektarynki commit

--- henio.js
+++ henio.js
@@ -1,25 +1,69 @@
-// 0 = idle, 1 = smack
+// 0 = idle, 1 = smack, 2 = talk, 
+// 3 = ne, 4 = kta, 5 = ryn, 6 = ki
 var henioState = 0
 
 const imgHenio = document.getElementById("henioImg");
-const aHenio = document.getElementById("henio")
+const aHenio = document.getElementById("henioA");
 
-const smack = document.getElementById('smack')
-const babel = document.getElementById('babel')
-const splat = document.getElementById('splat')
+const smack = document.getElementById("smackAud");
+const babel = document.getElementById("babelAud");
+const splat = document.getElementById("splatAud");
 
-imgHenio.addEventListener("click", () => {
-    if(henioState == 0){
-        imgHenio.src = "assets/babel/withTP/babelpunch.png"
-        smack.play()
-        henioState = henioState + 1
+
+const imgNektarynka = document.getElementById("nektarynkaImg");
+const aNektarynka = document.getElementById("nektarynkaA");
+
+const nekta = document.getElementById("nektarynkaAud");
+const fnaf = document.getElementById("fnafAud");
+
+
+
+aHenio.addEventListener("click", () => {
+    if (henioState == 0) {
+        henioState = henioState + 1;
+        imgHenio.src = "assets/babel/withTP/babelpunch.png";
+        smack.play();
         setTimeout(() => {
+            henioState = henioState + 1;
             imgHenio.src = "assets/babel/withTP/babeltalk.png";
-            babel.play()
+            babel.play();
             setTimeout(() => {
+                henioState = 0;
                 imgHenio.src = "assets/babel/withTP/babelstand.png";
-                henioState = 0
             }, 800);
-        }, 800);        
+        }, 800);
     }
 });
+
+setTimeout(() => {
+    aNektarynka.classList.add("move-in");
+}, 2000);
+
+aNektarynka.addEventListener("click", () => {
+    aNektarynka.classList.add("click");
+    setTimeout(() => {
+        aNektarynka.classList.remove("click");
+        fnaf.play();
+    }, 140);
+    if (henioState == 0) {
+        henioState = 3;
+        imgHenio.src = "assets/babel/nekta/nekta1.png";
+        nekta.play();
+        setTimeout(() => {
+            henioState = henioState + 1;
+            imgHenio.src = "assets/babel/nekta/nekta2.png";
+            setTimeout(() => {
+                henioState = henioState + 1;
+                imgHenio.src = "assets/babel/nekta/nekta3.png";
+                setTimeout(() => {
+                    henioState = henioState + 1;
+                    imgHenio.src = "assets/babel/nekta/nekta4.png";
+                    setTimeout(() => {
+                        henioState = 0;
+                        imgHenio.src = "assets/babel/withTP/babelstand.png";
+                    }, 600);
+                }, 700);
+            }, 600);
+        }, 700);
+    }
+})
